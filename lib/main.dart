1901eb104b612c480630b41d702f55ebd710ef4a
devsourcenathan <sadenachbinathan@gmail.com>
2022-12-13 11:36:29 +0100

    Add introductions screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
+import 'package:agro_learning/screens/home_screen.dart';
+import 'package:agro_learning/screens/iintroduction_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+bool show = true;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  show = prefs.getBool('ON_BOARDING') ?? true;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Agro Learning',
+      title: 'Flutter On Boarding',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: show ? IntroScreen() : const HomeScreen(),
+      home: IntroScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
